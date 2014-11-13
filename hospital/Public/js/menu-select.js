04a9601417357689c1ac8805b4ec7d85acc8bd9e
cscdoubt <zhangxiaoyu@gencare.me>
2014-11-13 23:23:09 +0800

    Delete
    Must delete

--- hospital/Public/js/menu-select.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-   $("#menu-code-index").ready(function(){
-				  var idx = $("#menu-code-index").val();
-				  	$('li[data-service-index="' + idx + '"]').addClass("active");
-			  });
-});
--- hospital/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('APP_DEBUG',TRUE); // 开启调试模式
-define('APP_NAME','App');
-define('APP_PATH','./App/');
-define('THINK_PATH','./ThinkPHP/');
-define('ENGINE_NAME','cluster');
-require THINK_PATH.'ThinkPHP.php';
